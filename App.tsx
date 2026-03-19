6985d81f1f6a5ff3c0c85dc596a6092734da1c1c
GabryOchoa <AleXyOchoa9@gmail.com>
2026-03-19 11:06:34 -0600

    chore: initial project scaffold

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-white text-xl font-bold">Shopping List APP 🛒</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,1 @@
+# shopping-list-clean-architecture
